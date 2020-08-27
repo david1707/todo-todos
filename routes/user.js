039a553bb89491f985076bf281195cbc92c9e650
david1707 <DavidAlacant@gmail.com>
2020-08-27 10:42:52 +0200

    Updated user.js route comments

--- routes/user.js
+++ routes/user.js
@@ -3,36 +3,36 @@ const express = require("express");
 const router = express.Router();
 
 // Create
-router.post("/create", (req, res, next) => {
+router.post("/", (req, res, next) => {
   //TODO Create new user
-  // Return created user
+  // Creates a new User
   res.json({ data: "User created" });
 });
 
 // Profile
 router.get("/profile", (req, res, next) => {
   //TODO Just return the own user stored on req.user. No other users may be requested
-  // Return own user
+  // Returns own User
   res.json({ data: "Current user data" });
 });
 
 router.patch("/profile", (req, res, next) => {
   //TODO Update Username, email and/or password and auto-update updatedAt
-  // Return updated user
+  // Updates User
   res.json({ data: "User updated" });
 });
 
 // Delete User
 router.delete("/delete", (req, res, next) => {
   //TODO Delete own user
-  // Return confirmation
+  // Deletes a User
   res.json({ data: "User deleted" });
 });
 
 // Methods related to Todos
 router.get("/todos", (req, res, next) => {
   //TODO Returns all the TODOS that the user can edit.
-
+  // Get all Todos that the user can edit (own and with access)
   res.json({
     data: [
       { id: 1, name: "Todo list #1", todos: [] },
@@ -46,7 +46,7 @@ router.get("/todos", (req, res, next) => {
 
 router.get("/own-todos", (req, res, next) => {
   //TODO Returns all the TODOS created by the user
-
+  // Get all Todos the user created
   res.json({
     data: [
       { id: 1, name: "Todo list #1", todos: [] },
