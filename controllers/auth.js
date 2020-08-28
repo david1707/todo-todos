a274f84afd789f62eb1e9ae1c6464bdb76438560
david1707 <DavidAlacant@gmail.com>
2020-08-28 10:27:54 +0200

    Created SignIn with hashed passsword

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
+
 exports.login = (req, res, next) => {
   //TODO Lets the user login with email and password
   // Confirms user login and returns a JWT
@@ -12,8 +16,28 @@ exports.logout = (req, res, next) => {
   res.json({ data: "User logout" });
 };
 
-exports.signIn = (req, res, next) => {
-  //TODO Create new user
+exports.signIn = async (req, res, next) => {
   // Creates a new User
-  res.json({ data: "User created" });
+
+  // Checks if user already exists
+  const { email, password } = req.body;
+  const userExists = await User.findOne({ email: email });
+
+  // If user already exists, exit
+  if (userExists) {
+    return res.json({ error: `The email '${email}' is already in use.` });
+  }
+
+  // Hash the password and then create an user
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = new User({
+    email: email,
+    password: hashedPassword,
+  });
+
+  await user.save();
+
+  res.status(201).json({ message: `User created with email '${email}'` });
 };
